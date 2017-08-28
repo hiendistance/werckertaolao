379c24edff7e2f0083170a51071d71d14043503e
hiendistance <hien.nguyen@staff.distance-c.asia>
2017-08-28 15:13:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $cities = array("Amsterdam", "San Francisco", "Berlin",
-                "New York", "Tokyo", "London");
+                "New York", "Tokyo", "London", "London tao lao");
 
 header('Content-Type: application/json');
 echo json_encode($cities, JSON_PRETTY_PRINT);
